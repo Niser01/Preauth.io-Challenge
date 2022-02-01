5832d1b411fddccea8856af3144d9620aa80ff2d
Sergio Nicolás <64512516+Niser01@users.noreply.github.com>
2022-02-01 14:36:27 -0500

    Add files via upload

--- Game 02/app/gilded-rose.ts
+++ Game 02/app/gilded-rose.ts
@@ -22,14 +22,17 @@ export class GildedRose {
         for (let i = 0; i < this.items.length; i++) {
             /*We use a swithc case with the names of each element, and we add or substract the qualitid acodingly to the product and the sellin time */
             switch (this.items[i].name) {
+                /*Since conjured degrades twice as fast under normal circunstances thats why quality is reduced by 2, but if the seling date has passed  */
                 case 'Conjured':
                     this.items[i].quality-=2
+                    this.items[i].sellIn-=1
                     if(this.items[i].sellIn<0){
                         this.items[i].quality-=2
                     }
                     break;
 
                 case 'Aged Brie':
+                    this.items[i].sellIn-=1
                     if(this.items[i].quality<50){
                         this.items[i].quality+=1
                     }
@@ -50,10 +53,12 @@ export class GildedRose {
                     }else{
                         this.items[i].quality=0
                     }
+                    this.items[i].sellIn-=1
                     break;  
 
                 default:
                     this.items[i].quality-=1
+                    this.items[i].sellIn-=1
                     if(this.items[i].sellIn<0){
                         this.items[i].quality-=1
                     }
