ca826009adcd0514e0f99027c21ea3b9e5da76df
Sergio Nicolás <64512516+Niser01@users.noreply.github.com>
2022-02-01 14:41:08 -0500

    Add files via upload

--- Game 02/app/gilded-rose.ts
+++ Game 02/app/gilded-rose.ts
@@ -22,7 +22,8 @@ export class GildedRose {
         for (let i = 0; i < this.items.length; i++) {
             /*We use a swithc case with the names of each element, and we add or substract the qualitid acodingly to the product and the sellin time */
             switch (this.items[i].name) {
-                /*Since conjured degrades twice as fast under normal circunstances thats why quality is reduced by 2, but if the seling date has passed  */
+                /*Since conjured degrades twice as fast under normal circunstances thats why quality is reduced by 2, but if the seling date has passed 
+                it decreses twice as fast */
                 case 'Conjured':
                     this.items[i].quality-=2
                     this.items[i].sellIn-=1
@@ -32,6 +33,7 @@ export class GildedRose {
                     break;
 
                 case 'Aged Brie':
+                    /*The cheese will never loose quality, but it can not be more then 50 */
                     this.items[i].sellIn-=1
                     if(this.items[i].quality<50){
                         this.items[i].quality+=1
@@ -39,9 +41,12 @@ export class GildedRose {
                     break;   
 
                 case 'Sulfuras, Hand of Ragnaros':
+                    /*Since sulfuras is a lengendary item, it will not loose quality or hace a selling date */
                     break;  
 
                 case 'Backstage passes to a TAFKAL80ETC concert':
+                    /*The concert will gain quality for each day that is closer to the concert date, if its less thn 10 days it will cost
+                    double or if its less than 5 days it will triple the value, but it the date has passed the quality will be 0 */
                     if(this.items[i].sellIn>0){
                         this.items[i].quality+=1
                         if (this.items[i].sellIn<11) {
@@ -57,6 +62,8 @@ export class GildedRose {
                     break;  
 
                 default:
+                    /*All the other items will have a regular degration of quality and selling time, but if the selling date has passed, the value 
+                    will be reduced by 2 */
                     this.items[i].quality-=1
                     this.items[i].sellIn-=1
                     if(this.items[i].sellIn<0){
