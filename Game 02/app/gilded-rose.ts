1fca09aa79f329db8105af25fbf086158af9c595
Sergio Nicolás <64512516+Niser01@users.noreply.github.com>
2022-02-01 14:30:26 -0500

    Add files via upload

--- Game 02/app/gilded-rose.ts
+++ Game 02/app/gilded-rose.ts
@@ -20,47 +20,45 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            /*In this first if statement, by default to all items in the array the quality of the item will be reduced by 1 and by 2 in the case 
-            of conjured items*/
-            if (this.items[i].quality>0) {
-                this.items[i].quality-=1
-                if(this.items[i].name=='Conjured'){
-                    this.items[i].quality-=1
-                }
-            }
-            /*Then we take into acount the lengendary and the products that actually get better with time, and we add the quality 
-            point that was taken in the first staement.
-            The we check if they meet the criteria of quality under 50, and also add the extra quality point 
-            and in the case of the concert we add also the extra 2 or 3 quality point that are given due to the date of the concert */
-            if (this.items[i].name == 'Aged Brie' || this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert' ||  this.items[i].name =='Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality+=1
-                if(this.items[i].quality<50){
-                    this.items[i].quality+=1
-                    if(this.items[i].sellIn<11 && this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
+            /*We use a swithc case with the names of each element, and we add or substract the qualitid acodingly to the product and the sellin time */
+            switch (this.items[i].name) {
+                case 'Conjured':
+                    this.items[i].quality-=2
+                    if(this.items[i].sellIn<0){
+                        this.items[i].quality-=2
+                    }
+                    break;
+
+                case 'Aged Brie':
+                    if(this.items[i].quality<50){
                         this.items[i].quality+=1
-                        if(this.items[i].sellIn<6){
+                    }
+                    break;   
+
+                case 'Sulfuras, Hand of Ragnaros':
+                    break;  
+
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    if(this.items[i].sellIn>0){
+                        this.items[i].quality+=1
+                        if (this.items[i].sellIn<11) {
                             this.items[i].quality+=1
+                            if (this.items[i].sellIn<6) {
+                                this.items[i].quality+=1
+                            }
                         }
-                    }
-                }
-            }
-            /*In this final set of conditionals, we check the sellIn dates and reduce them acordingly to the 
-            product specifications, in the case of 'Sulfuras, no date will be reduce since this is the legenday item
-            otherwise the date will be reduced by 1, and since we are checking the dates, we also reduce the quality of the products
-            that are expired and also place the concert to 0 if the date has passed.*/
-            if (this.items[i].name !='Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn-=1
-                if(this.items[i].sellIn<0 && this.items[i].quality>0){
-                    this.items[i].quality-=1
-                    if (this.items[i].name=='Conjured') {
-                        this.items[i].quality-=1
-                    }
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    }else{
                         this.items[i].quality=0
                     }
-                    
-                }
-            }            
+                    break;  
+
+                default:
+                    this.items[i].quality-=1
+                    if(this.items[i].sellIn<0){
+                        this.items[i].quality-=1
+                    }
+                    break;
+            }        
         }
         /*Here we give back the full array with the new specifications of quality and selling dates. */
         return this.items;
